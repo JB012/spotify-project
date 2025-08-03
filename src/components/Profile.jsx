ee636cd7547785f978a5d4abd9ed19b64bce8798
Jamal Bankole <jbankole@terpmail.umd.edu>
2025-08-03 17:40:22 -0400

    Added top tracks/artists and option to toggle them based on time range

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,50 +3,71 @@ import axios from 'axios'
 import useAuth from "./useAuth";
 import '../App.css'
 
+function pad(num) {
+    return (num < 10) ? '0' + num.toString() : num.toString();
+}
+
+function convertDurationToMinSec(duration_ms) {
+    const seconds = duration_ms / 1000;
+    return `${Math.floor(seconds / 60)}:${pad(Math.floor(seconds % 60))}`;
+}
 function Profile({code}) {
     //Writing as a custom Hook deduplicates requests and cache responses between components. 
     const {data: userData} = useAuth(code);
     const [topArtists, setTopArtists] = useState([]);
-    
-    useEffect(() => {
-        if (userData !== '') {
-            axios.get("https://api.spotify.com/v1/me/top/artists", {
+    const [topTracks, setTopTracks] = useState([]);
+    const [timeRange, setTimeRange] = useState("");
+
+    function handleTimeRange(event) {
+        const timeRange = event.target.value;
+        if (timeRange !== "") {
+            event.target.style.color = "white";
+
+            axios.get(`https://api.spotify.com/v1/me/top/artists?time_range=${timeRange}`, {
                 headers: {
                     Authorization: `Bearer ${userData["access_token"]}`
                 }
             }).then(topArtists => {
-                setTopArtists(topArtists.data.items);
+                axios.get(`https://api.spotify.com/v1/me/top/tracks?time_range=${timeRange}`, {
+                    headers: {
+                        Authorization: `Bearer ${userData["access_token"]}`
+                    }
+                }).then(topTracks => {
+                    setTimeRange(timeRange);    
+                    setTopArtists(topArtists.data.items);
+                    setTopTracks(topTracks.data.items);
+                }).catch(err => console.log(err.response.data));
+            }).catch(e => console.log(e.response.data));
+        }
+    }
+
+    useEffect(() => {
+        if (userData !== '') {
+            axios.get("https://api.spotify.com/v1/me/top/artists?time_range=short_term", {
+                headers: {
+                    Authorization: `Bearer ${userData["access_token"]}`
+                }
+            }).then(topArtists => {
+                axios.get("https://api.spotify.com/v1/me/top/tracks?time_range=short_term", {
+                    headers: {
+                        Authorization: `Bearer ${userData["access_token"]}`
+                    }
+                }).then(topTracks => {    
+                    setTopArtists(topArtists.data.items);
+                    setTopTracks(topTracks.data.items);
+                }).catch(err => console.log(err.response.data));
             }).catch(e => console.log(e.response.data));
         }
     }, [userData]);
 
-    /* useEffect(() => {
-        if (authenticationData.accessToken !== "") {
-            axios.get("https://api.spotify.com/v1/me", {
-                headers: {
-                    Authorization: `Bearer ${authenticationData.accessToken}`
-                }
-            })
-            .then(jsonData => setUserData({data: jsonData.data}))
-            .catch(err => {
-                    console.log(err.response.data);
-                    if (err.response.status === "401") {
-                        axios.post("http://[::1]:3001/refresh", {accessToken: authenticationData.accessToken})
-                        .then(tokens => {
-                            authenticationData.accessToken = tokens.accessToken;
-                            authenticationData.refreshToken = tokens.refreshToken;
-                        })
-                        .catch(err => console.log(err.response.data))
-                    }
-                });
-                
-        }
-    }, [authenticationData, userData]); */
-
     return (
-        <>
-            <div className="container w-full h-full">
-                <div id="header-container" className="flex flex-col gap-y-2 justify-center items-center w-full h-full">
+        <div className="flex">
+            <nav>
+                <a href="#">Profile</a>
+                <a href="#">Player</a>
+            </nav>
+            <div className="container w-full h-full pl-50 pt-20 pr-20">
+                <header id="header-container" className="flex flex-col gap-y-10 justify-center items-center w-full h-full">
                     <div className="text-3xl font-bold">
                         {userData["display_name"]}
                     </div>
@@ -60,9 +81,60 @@ function Profile({code}) {
                             <div className="sub-text">FOLLOWING</div>
                         </div>
                     </div>
+                </header>
+                <div className="flex w-full justify-end">
+                    <select onChange={(e) => handleTimeRange(e)}>
+                        <option style={{color: timeRange === "short_term" ? "white" : "gray"}} value="short_term">Short Term</option>
+                        <option style={{color: timeRange === "medium_term" ? "white" : "gray"}} value="medium_term">Medium Term</option>
+                        <option style={{color: timeRange === "long_term" ? "white" : "gray"}} value="long_term">Long Term</option>
+                    </select>
                 </div>
+                <section id="top-container" className="flex justify-around">
+                    <div id="top-artists" className="flex flex-col grow w-full">
+                        <div>Top Artists</div>
+                        <ul id="top-artists-list" className="w-full">
+                        {
+                                topArtists.map((topArtist) => 
+                                <li key={topArtist.id} className="top-artists-item">
+                                    <img width='50px' height='50px' className="rounded-full" src={topArtist.images[2].url} />
+                                    <div>{topArtist.name}</div>
+                                </li>)
+                            }
+                        </ul>
+                    </div>
+                    <div id="top-songs" className="flex flex-col grow w-full">
+                        <div>Top Tracks</div>
+                        <ul id="top-songs-list" className="w-full">
+                            {
+                                topTracks.map((topTrack) => 
+                                    <li key={topTrack.id} className="top-tracks-item">
+                                        <img src={topTrack.album.images[2].url}/>
+                                        <div className="flex flex-col w-full">
+                                            <div className="flex justify-between">
+                                                <div>{topTrack.name}</div>
+                                                <div className="sub-text text-center">
+                                                    {
+                                                       convertDurationToMinSec(topTrack["duration_ms"])
+                                                    }
+                                                </div>
+                                            </div>
+                                            <div className="sub-text">
+                                                {
+                                                    topTrack.album.artists.map((artist) => artist === topTrack.album.artists[topTrack.album.artists.length - 1] ? `${artist.name} ` : `${artist.name}, `)
+                                                }
+                                                &middot; 
+                                                {` ${topTrack.album.name}`}
+                                                
+                                            </div>
+                                        </div>
+                                    </li>
+                                )
+                            }
+                        </ul>
+                    </div>
+                </section>
             </div>
-        </>
+        </div>
     )
 };
 export default Profile
